877e8454c35372c473e2be0ac4d9f7f4d6e1f19e
Fahad Salim <85647102+fahadsalim02@users.noreply.github.com>
2021-12-20 12:48:36 +0530

    Update Exception_handling.java

--- Lab4/Exception_handling.java
+++ Lab4/Exception_handling.java
@@ -1,30 +1,29 @@
 package oopj201221;
-public class Exception_handling {
-
+import java.util.*;
+public class ExceptionHandling{
 public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-int x=0;
-do {
-System.out.println("--Program to perform division--\n");
-System.out.println("Enter number 1: ");
-int num1=sc.nextInt();
-try {
-System.out.println("Enter number 2: ");
-int num2=sc.nextInt();
-System.out.println(num1/num2);
+{
+   Scanner sc=new Scanner(System.in);{
+   int x=0;
+     {
+	   System.out.println("---Program to preform division---\n");
+       System.out.println("Enter the number 1");
+       int num1=sc.nextInt();
+       try 
+         {
+	     System.out.println("entrer the number 2");
+         int num2=sc.nextInt();
+         System.out.println(num1/num2);}
+         catch(ArithmeticException e) {
+         System.out.println("Do not divide by zero!!!");
+         }
+           finally 
+            {
+      }      System.out.println("End of Operations");
 }
-catch(ArithmeticException e) {
-System.out.println("Do not divide by Zero!!!");
-}
-finally {
-System.out.println("End of Operations");
-}
-System.out.println("Do you want to continue(y/n)?");
-char c=sc.next().charAt(0);
-if(c=='Y'||c=='y')
-x++;
-}while(x!=0);
+   }
 
-}
 
+ }
+}
 }
